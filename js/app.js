e7852c730ab425c1004da16ebf7bd812fe11b29e
kmangutov <kmangutov@gmail.com>
2015-09-05 17:25:41 -0400

    change to pastels

--- js/app.js
+++ js/app.js
@@ -34,22 +34,27 @@ function setColorThree(stringHex) {
   setColorIndex(stringHex, "three")
 };
 
-var hexes = [
-  "#006688",
-  "#009977",
-  "#990066",
-  "#991100"
-];
+function setColorScheme(arrayScheme) {
+  setColorOne(arrayScheme[0]);
+  setColorTwo(arrayScheme[1]);
+  setColorThree(arrayScheme[2]);
+}
 
+var hexCombinations = [
+  ["#acc7dc", "#d6dde3", "#ffffff"],
+  ["#f4858e", "#bfe2ca", "#a6daef"],
+  ["#676c7f", "#ca8799", "#e1d5bf"]
+];
 var hexIndex = 0;
 
 //test loop to cycle through colors
 var funcLoop = function() {
-  setColorOne(hexes[(hexIndex++) % hexes.length]);
+/*  setColorOne(hexes[(hexIndex++) % hexes.length]);
   setColorTwo(hexes[(hexIndex + 1) % hexes.length]);
-  setColorThree(hexes[(hexIndex + 2) % hexes.length]);
+  setColorThree(hexes[(hexIndex + 2) % hexes.length]);*/
 
-  setTimeout(funcLoop, 2000);
+  setColorScheme(hexCombinations[hexIndex++ % hexCombinations.length]);
+  setTimeout(funcLoop, 3500);
 }
 
 funcLoop();
