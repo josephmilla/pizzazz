09c0aa1fcf688ffbd74b852f8e3af220b330a0b7
Joseph Milla <jjtmilla@gmail.com>
2015-09-05 19:40:31 -0400

    Add documentation

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ var endpointIndexTitle = 'Pizzazz API &middot; ';
 
 /**
 * Test Endpoint
-* @param
-* @return
+* @param: null
+* @return: Hello, World!
 **/
 var testEndpoint = '/api/test';
 app.get(testEndpoint, function(req, res, next) {
@@ -39,8 +39,12 @@ app.get(testEndpoint, function(req, res, next) {
 
 /**
 * Complementary Endpoint
-* @param
-* @return
+* @description: Complementary -- Colors that are opposite each other on the color wheel are considered to be complementary colors (example: red and green).
+* The high contrast of complementary colors creates a vibrant look especially when used at full saturation. This color scheme must be managed well so it is not jarring.
+* Complementary colors are tricky to use in large doses, but work well when you want something to stand out.
+* Complementary colors are really bad for text.
+* @param: Color Hex Code (i.e. #000000)
+* @return: Complementary Color Hex Code (i.e. #000000 >> #FFFFFF)
 **/
 var complementaryEndpoint = '/api/complementary';
 app.get(complementaryEndpoint, function(req, res, next) {
@@ -64,8 +68,12 @@ app.get(complementaryEndpoint, function(req, res, next) {
 
 /**
 * Analogous Endpoint
-* @param
-* @return
+* @description: Analogous -- Analogous color schemes use colors that are next to each other on the color wheel. They usually match well and create serene and comfortable designs.
+* Analogous color schemes are often found in nature and are harmonious and pleasing to the eye.
+* Make sure you have enough contrast when choosing an analogous color scheme.
+* Choose one color to dominate, a second to support. The third color is used (along with black, white or gray) as an accent.
+* @param: Color Hex Code (i.e. #000000)
+* @return: Analogous Color Hex Codes
 **/
 var analogousEndpoint = '/api/analogous';
 app.get(analogousEndpoint, function(req, res, next) {
@@ -89,8 +97,11 @@ app.get(analogousEndpoint, function(req, res, next) {
 
 /**
 * Triad Endpoint
-* @param
-* @return
+* @description: Triad -- A triadic color scheme uses colors that are evenly spaced around the color wheel.
+* Triadic color harmonies tend to be quite vibrant, even if you use pale or unsaturated versions of your hues.
+* To use a triadic harmony successfully, the colors should be carefully balanced - let one color dominate and use the two others for accent.
+* @param: Color Hex Code (i.e. #000000)
+* @return: Triadic Color Hex Codes
 **/
 var triadEndpoint = '/api/triad';
 app.get(triadEndpoint, function(req, res, next) {
@@ -114,8 +125,11 @@ app.get(triadEndpoint, function(req, res, next) {
 
 /**
 * Split-Complementary Endpoint
-* @param
-* @return
+* @description: Split-Complementary -- The split-complementary color scheme is a variation of the complementary color scheme. In addition to the base color, it uses the two colors adjacent to its complement.
+* This color scheme has the same strong visual contrast as the complementary color scheme, but has less tension.
+* The split-complimentary color scheme is often a good choice for beginners, because it is difficult to mess up.
+* @param:  Color Hex Code (i.e. #000000)
+* @return: Split-Complementary Color Hex Codes
 **/
 var splitEndpoint = '/api/split';
 app.get(splitEndpoint, function(req, res, next) {
@@ -139,8 +153,12 @@ app.get(splitEndpoint, function(req, res, next) {
 
 /**
 * Rectangle Endpoint
-* @param
-* @return
+* @description: Rectangle -- The rectangle or tetradic color scheme uses four colors arranged into two complementary pairs.
+* This rich color scheme offers plenty of possibilities for variation.
+* The tetradic color scheme works best if you let one color be dominant.
+* You should also pay attention to the balance between warm and cool colors in your design.
+* @param: Color Hex Code (i.e. #000000)
+* @return: Rectangular Color Hex Codes
 **/
 var rectangleEndpoint = '/api/rectangle';
 app.get(rectangleEndpoint, function(req, res, next) {
@@ -164,8 +182,11 @@ app.get(rectangleEndpoint, function(req, res, next) {
 
 /**
 * Square Endpoint
-* @param
-* @return
+* @description: Square -- The square color scheme is similar to the rectangle, but with all four colors spaced evenly around the color circle.
+* The square color scheme works best if you let one color be dominant.
+* You should also pay attention to the balance between warm and cool colors in your design.
+* @param: Color Hex Code (i.e. #000000)
+* @return: Square Color Hex Codes
 **/
 var squareEndpoint = '/api/square';
 app.get(squareEndpoint, function(req, res, next) {
